ce576f1f9ac4ce7c93cc12709db651d3b7b9bcdb
HTMLCSSExplorer <htmlcssexplorer@gmail.com>
2024-03-30 20:46:52 -0500

    Dad jokes

---
+++ dadJokes/index.js
@@ -0,0 +1,26 @@
+const btn = document.querySelector('.btn');
+
+const endPoint = `https://icanhazdadjoke.com`;
+const options = {
+  headers: {
+    Accept: 'application/json',
+  },
+};
+const getJoke = async () => {
+  const res = await fetch(endPoint, options);
+  try {
+    if (!res.ok) {
+      throw new Error('Something went Wrong!');
+    }
+    const newJoke = await res.json();
+    document.querySelector('.small').innerText = `Joke ID:  ${newJoke.id}`;
+    document.querySelector('#joke').innerText = newJoke.joke;
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+btn.addEventListener('click', (e) => {
+  e.preventDefault();
+  getJoke();
+});
