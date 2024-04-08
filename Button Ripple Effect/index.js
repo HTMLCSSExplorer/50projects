c83c6ccc36b671917254b22a7b0d1eec0d308aa3
HTMLCSSExplorer <htmlcssexplorer@gmail.com>
2024-04-07 20:45:33 -0500

    button Effect

---
+++ Button Ripple Effect/index.js
@@ -0,0 +1,27 @@
+const btns = document.querySelectorAll('.btn');
+
+function myFunction(e) {
+  const x = e.clientX;
+  const y = e.clienty;
+
+  const btnTop = e.target.offsetTop;
+  const btnLeft = e.target.offsetLeft;
+
+  const xInside = x - btnLeft;
+  const yInside = y - btnTop;
+
+  const circle = document.createElement('span');
+  circle.classList.add('circle');
+  circle.style.left = xInside + 'px';
+  circle.style.top = yInside + 'px';
+
+  this.appendChild(circle);
+
+  setTimeout(() => {
+    circle.remove();
+  }, 500);
+}
+
+btns.forEach((btn) => {
+  btn.addEventListener('click', myFunction);
+});
